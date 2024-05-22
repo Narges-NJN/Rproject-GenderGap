df20a2cecc2fbf16b71e6e2996bcc58d8af1546d
narges najian <nargess.najian@gmail.com>
2024-05-22 13:08:34 +0200

    Computed the gap

--- GenderGap.R
+++ GenderGap.R
@@ -1,2 +1,18 @@
-selecteddata <- read.csv("lfsi_emp_a__custom_11484725_linear.csv")
-head(selecteddata)
+#install the data table package if you don't have it
+library(data.table)
+#loading the dataset
+dt <- fread("lfsi_emp_a__custom_11491788_linear.csv")
+#removing the columns that are not needed
+dt  <- dt[ , -2] 
+dt  <- dt[ , -2]
+dt  <- dt[ , -1] 
+dt  <- dt[ , -4]
+dt  <- dt[ , -7] 
+dt  <- dt[ , -1] 
+head(dt)
+
+gapDT <- dcast(dt, age + geo + TIME_PERIOD ~ sex, value.var = "OBS_VALUE")
+
+# Calculate the difference and create a new column
+gapDT[, gap := M - F]
+
